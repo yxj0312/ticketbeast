16e9745ff17f38f3ba89ab804e579301fa423b71
yxj0312 <yanxiajun@hotmail.com>
2018-09-03 22:15:42 +0200

    Chapter 2 Ep 3 Outlining the First Purchasing Test

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Concert;
 use Tests\TestCase;
+use App\Billing\PaymentGateway;
+use App\Billing\FakePaymentGateway;
 use Illuminate\Foundation\Testing\WithFaker;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PurchaseTicketsTest extends TestCase
@@ -13,25 +17,31 @@ class PurchaseTicketsTest extends TestCase
     /** @test */
     function customer_can_purchase_concert_tickets()
     {
+        $paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $paymentGateway);
+        
         // Arrange
         // Create a concert
         $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
 
         // Act
         // Purchase concert tickets
-        $this->json('POST',"/concerts/{$concert->id}/orders", [
+        $response = $this->json('POST',"/concerts/{$concert->id}/orders", [
             'email' => 'john@example.com',
             'ticket_quantity' => 3,
             'payment_token' => $paymentGateway->getValidTestToken(), 
         ]);
 
         // Assert
+        $response->assertStatus(201);
+
+
         // Make sure the customer was changed the correct amount
         $this->assertEquals(9750, $paymentGateway->totalCharges());
 
         // Make sure that an order exists for this customer
         $order = $concert->orders()->where('email', 'john@example.com')->first();
-        $this->assertNotNull($order);
-        $this->assertEquals(3, $order->tickets->count());
+        $response->assertNotNull($order);
+        $response->assertEquals(3, $order->tickets->count());
     }
 }
---
+++ tests/Unit/Billing/FakePaymentGatewayTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Billing;
+
+use Tests\TestCase;
+use App\Billing\FakePaymentGateway;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FakePaymentGatewayTest extends TestCase
+{
+    /** @test */
+    function charges_with_a_valid_payment_token_are_successful()
+    {
+        $paymentGateway = new FakePaymentGateway;
+
+        $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+
+        $this->assertEquals(2500, $paymentGateway->totalCharges());
+    }
+}
