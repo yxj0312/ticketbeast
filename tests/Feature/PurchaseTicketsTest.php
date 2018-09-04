4c714aebc038670e2544e05ba20009dac121908b
yxj0312 <yanxiajun@hotmail.com>
2018-09-04 21:22:54 +0200

    Chapter 2 Ep 5 Adding Tickets to Orders

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -41,7 +41,7 @@ class PurchaseTicketsTest extends TestCase
 
         // Make sure that an order exists for this customer
         $order = $concert->orders()->where('email', 'john@example.com')->first();
-        $response->assertNotNull($order);
-        $response->assertEquals(3, $order->tickets->count());
+        $this->assertNotNull($order);
+        $this->assertEquals(3, $order->tickets()->count());
     }
 }
