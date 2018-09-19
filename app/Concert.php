460a9458bcf92b5f34b8f554afc1870398459ca7
XiajunYan <yanxiajun@hotmail.com>
2018-09-19 18:05:36 +0200

    Chapter 5 Ep 2 Removing the Need to Cancel Orders

--- app/Concert.php
+++ app/Concert.php
@@ -52,14 +52,26 @@ class Concert extends Model
 
     public function orderTickets($email, $ticketQuantity)
     {
-        $tickets = $this->tickets()->available()->take($ticketQuantity)->get();
-        if ($tickets->count() < $ticketQuantity) {
+        $tickets = $this->findTickets($ticketQuantity);
+        return $this->createOrder($email, $tickets);
+    }
+
+    public function findTickets($quantity)
+    {
+        $tickets = $this->tickets()->available()->take($quantity)->get();
+
+        if ($tickets->count() < $quantity) {
             throw new NotEnoughTicketsException();
         }
 
+        return $tickets;
+    }
+
+    public function createOrder($email, $tickets)
+    {
         $order = $this->orders()->create([
             'email' => $email,
-            'amount' => $ticketQuantity * $this->ticket_price,
+            'amount' => $tickets->count() * $this->ticket_price,
         ]);
 
         foreach ($tickets as $ticket) {
@@ -68,7 +80,7 @@ class Concert extends Model
 
         return $order;
     }
-
+    
     public function addTickets($quantity)
     {
         foreach (range(1, $quantity) as $i) {
@@ -81,5 +93,6 @@ class Concert extends Model
     public function ticketsRemaining()
     {
         return $this->tickets()->available()->count();
-    } 
+    }
+
 }
