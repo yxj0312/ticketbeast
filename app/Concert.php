e8be055475e8f2dcfd7323d55803b04aa9f57c78
XiajunYan <yanxiajun@hotmail.com>
2018-09-20 16:32:33 +0200

    Chapter 5 Ep 3 Preparing for Extraction

--- app/Concert.php
+++ app/Concert.php
@@ -37,7 +37,7 @@ class Concert extends Model
 
     public function orders()
     {
-        return $this->hasMany(Order::class);
+        return $this->belongsToMany(Order::class, 'tickets');
     }
 
     public function hasOrderFor($customerEmail)
@@ -69,10 +69,10 @@ class Concert extends Model
 
     public function createOrder($email, $tickets)
     {
-        $order = $this->orders()->create([
+        $order = Order::create([
             'email' => $email,
-            'amount' => $tickets->count() * $this->ticket_price,
-        ]);
+            'amount' => $tickets->sum('price'),
+        ]); 
 
         foreach ($tickets as $ticket) {
             $order->tickets()->save($ticket);
