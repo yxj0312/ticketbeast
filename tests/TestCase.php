da0fe803c94350041b4586bf2f0910fbd766eac7
XiajunYan <yanxiajun@hotmail.com>
2018-10-02 12:14:17 +0200

    Remove helper function of  withExceptionHandling();

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,28 +17,5 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         Schema::enableForeignKeyConstraints();
-
-        $this->disableExceptionHandling();
-    }
-
-    // Hat tip, @adamwathan.
-    protected function disableExceptionHandling()
-    {
-        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
-
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct() {}
-            public function report(\Exception $e) {}
-            public function render($request, \Exception $e) {
-                throw $e;
-            }
-        });
-    }
-
-    protected function withExceptionHandling()
-    {
-        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
-
-        return $this;
     }
 }
