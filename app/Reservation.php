4df247212363c2b0e14225e61a429c17ff4ddda5
yxj0312 <yanxiajun@hotmail.com>
2018-10-14 19:01:15 +0000

    Apply fixes from StyleCI

--- app/Reservation.php
+++ app/Reservation.php
@@ -38,7 +38,7 @@ class Reservation
     public function complete($paymentGateway, $paymentToken)
     {
         $paymentGateway->charge($this->totalCost(), $paymentToken);
-        
+
         return Order::forTickets($this->tickets(), $this->email(), $this->totalCost());
     }
 }
