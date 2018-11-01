4d2606a577d6ba404391e8b72909b52ca074aa49
yxj0312 <yanxiajun@hotmail.com>
2018-11-01 18:50:17 +0100

    Chapter 12 Ep 7 Decoupling Data from Presentation

--- tests/Feature/ViewOrderTest.php
+++ tests/Feature/ViewOrderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Order;
 use App\Ticket;
 use App\Concert;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -18,12 +19,23 @@ class ViewOrderTest extends TestCase
     {
         $this->withoutExceptionHandling();
         // Create an concert
-        $concert = factory(Concert::class)->create(); 
+        $concert = factory(Concert::class)->create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('March 12, 2017 8:00pm'),
+            'ticket_price' => 4250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+        ]); 
         // Create an Order
         $order = factory(Order::class)->create([
             'confirmation_number' => 'ORDERCONFIRMATION1234',
             'card_last_four' => '1881',
             'amount' => 8500,
+            'email' => 'john@example.com',
         ]); 
         // Create a ticket
         $ticketA = factory(Ticket::class)->create([
@@ -53,5 +65,15 @@ class ViewOrderTest extends TestCase
         $response->assertSee('**** **** **** 1881');
         $response->assertSee('TICKETCODE123');
         $response->assertSee('TICKETCODE456');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON');
+        $response->assertSee('17916');
+        $response->assertSee('john@example.com');
+
+        $response->assertSee('2017-03-12 20:00');
+        $response->assertSee('Doors at 8:00pm');
     }
 }
