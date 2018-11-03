c6e9062e284bd2988c095cda5b461403448ef107
yxj0312 <yanxiajun@hotmail.com>
2018-11-03 19:56:11 +0000

    Apply fixes from StyleCI

--- app/Order.php
+++ app/Order.php
@@ -46,7 +46,7 @@ class Order extends Model
     public function toArray()
     {
         return [
-            'confirmation_number' => $this->confirmation_number, 
+            'confirmation_number' => $this->confirmation_number,
             'email' => $this->email,
             'ticket_quantity' => $this->ticketQuantity(),
             'amount' => $this->amount,
