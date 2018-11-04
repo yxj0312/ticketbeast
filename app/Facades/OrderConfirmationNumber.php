3fc63542d464df93ee27b7ec9e75a2d6dbcf6063
yxj0312 <yanxiajun@hotmail.com>
2018-11-04 19:34:58 +0000

    Apply fixes from StyleCI

--- app/Facades/OrderConfirmationNumber.php
+++ app/Facades/OrderConfirmationNumber.php
@@ -5,7 +5,6 @@ namespace App\Facades;
 use Illuminate\Support\Facades\Facade;
 use App\OrderConfirmationNumberGenerator;
 
-
 class OrderConfirmationNumber extends Facade
 {
     protected static function getFacadeAccessor()
