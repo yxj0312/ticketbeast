23c21f226f87e5b525c203aa1c48c0b9c5e0a763
yxj0312 <yanxiajun@hotmail.com>
2018-11-04 20:34:46 +0100

    Chapter 13 Ep 7 Refactoring to a Facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use App\Billing\PaymentGateway;
 use App\Billing\StripePaymentGateway;
 use Illuminate\Support\ServiceProvider;
 
+use App\OrderConfirmationNumberGenerator;
+use App\RandomOrderConfirmationNumberGenerator;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -30,5 +33,6 @@ class AppServiceProvider extends ServiceProvider
         });
 
         $this->app->bind(PaymentGateway::class, StripePaymentGateway::class);
+        $this->app->bind(OrderConfirmationNumberGenerator::class, RandomOrderConfirmationNumberGenerator::class);
     }
 }
