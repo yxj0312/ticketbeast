c4b3656233bac9309894592ae006777fc5514014
yxj0312 <yanxiajun@hotmail.com>
2018-11-10 20:37:33 +0100

    Chapter 15 Ep 3 Assigning Codes When Claiming Tickets

--- tests/Unit/TicketTest.php
+++ tests/Unit/TicketTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Unit;
 
+use App\Order;
 use App\Ticket;
 use App\Concert;
+use Carbon\Carbon;
 use Tests\TestCase;
+use App\Facades\TicketCode;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Carbon\Carbon;
 
 class TicketTest extends TestCase
 {
@@ -33,5 +35,22 @@ class TicketTest extends TestCase
         $ticket->release();
 
         $this->assertNull($ticket->fresh()->reserved_at);
+    }
+
+    /** @test */
+    function a_ticket_can_be_claimed_for_an_order()
+    {
+        $order = factory(Order::class)->create();
+        $ticket = factory(Ticket::class)->create(['code' => null]);
+        TicketCode::shouldReceive('generate')->andReturn('TICKETCODE1');
+        $this->assertNull($ticket->code);
+
+        $ticket->claimFor($order);
+
+        // Assert that the ticket is saved to the order
+        $this->assertContains($ticket->id, $order->tickets->pluck('id'));
+        $this->assertEquals('TICKETCODE1', $ticket->code);
+        // Assert that the ticket had the expected ticket code generated
+
     }
 }
