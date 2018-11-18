d667385e997dd3d401c5bb556de63485dfdc9779
yxj0312 <yanxiajun@hotmail.com>
2018-11-18 20:15:10 +0100

    Chapter 15 Ep 1 Testing the Login Endpoint

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/concerts/{id}', 'ConcertsController@show');
-
 Route::post('/concerts/{id}/orders', 'ConcertOrdersController@store');
-
 Route::get('/orders/{confirmationNumber}', 'OrdersController@show');
+
+Route::post('/login', 'Auth\LoginController@login');
---
+++ tests/Feature/PromoterLoginTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use Auth;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PromoterLoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function logging_in_with_valid_credentials()
+    {
+        $this->withoutExceptionHandling();
+        
+        $user = factory(User::class)->create([
+            'email' => 'jane@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'jane@example.com',
+            'password' => 'super-secret-password',
+        ]);
+
+        $response->assertRedirect('/backstage/concerts'); 
+        $this->assertTrue(Auth::check());
+        $this->assertTrue(Auth::user()->is($user));
+    }
+
+    /** @test */
+    function logging_in_with_invalid_credentials()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create([
+            'email' => 'jane@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'jane@example.com',
+            'password' => 'not-the-right-password',
+        ]);
+
+        $response->assertRedirect('/login'); 
+        $response->assertSessionHasErrors('email');
+        $this->assertFalse(Auth::check());
+    }
+
+    /** @test */
+    function logging_in_with_an_account_that_does_not_exist()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/login', [
+            'email' => 'nobody@example.com',
+            'password' => 'not-the-right-password',
+        ]);
+
+        $response->assertRedirect('/login'); 
+        $response->assertSessionHasErrors('email');
+        $this->assertFalse(Auth::check());
+    }
+}
