b9a5315715f55e5161c3a0b4b87efb65c3ce094b
yxj0312 <yanxiajun@hotmail.com>
2018-11-18 20:58:55 +0100

    Chapter 15 Ep 4 Getting Started with Laravel Dusk

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function(){
+    return "Laravel";
+});
+
 
 Route::get('/concerts/{id}', 'ConcertsController@show');
 Route::post('/concerts/{id}/orders', 'ConcertOrdersController@store');
