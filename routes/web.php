924cf991f8780d2e6692b433008afe944d9423bf
yxj0312 <yanxiajun@hotmail.com>
2018-11-18 21:18:13 +0100

    Remove unused route

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function(){
-    return "Laravel";
-});
-
-
 Route::get('/concerts/{id}', 'ConcertsController@show');
 Route::post('/concerts/{id}/orders', 'ConcertOrdersController@store');
 Route::get('/orders/{confirmationNumber}', 'OrdersController@show');
