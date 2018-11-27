a82f44b7cd67e3b578085bc398cb17ecdc67c4e1
XiajunYan <yanxiajun@hotmail.com>
2018-11-27 18:03:41 +0100

    Chapter 18 Ep 1 Viewing the Update Form -2:43

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use App\Exceptions\Handler;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -19,5 +20,9 @@ abstract class TestCase extends BaseTestCase
         Schema::enableForeignKeyConstraints();
 
         \Mockery::getConfiguration()->allowMockingNonExistentMethods(false);
+
+        TestResponse::macro('data', function ($key) {
+            return $this->original->getData()[$key];
+        });   
     }
 }
