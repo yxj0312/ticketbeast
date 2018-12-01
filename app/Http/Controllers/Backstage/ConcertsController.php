d9cf9ec4867ccdf2046046a962894fd9c0919111
yxj0312 <yanxiajun@hotmail.com>
2018-12-01 20:35:39 +0100

    Apply fixes from StyleCI (#42)

--- app/Http/Controllers/Backstage/ConcertsController.php
+++ app/Http/Controllers/Backstage/ConcertsController.php
@@ -98,7 +98,7 @@ class ConcertsController extends Controller
             'state' => request('state'),
             'zip' => request('zip'),
             'ticket_price' => request('ticket_price') * 100,
-            'ticket_quantity' => (int)request('ticket_quantity'),
+            'ticket_quantity' => (int) request('ticket_quantity'),
         ]);
 
         return redirect()->route('backstage.concerts.index');
