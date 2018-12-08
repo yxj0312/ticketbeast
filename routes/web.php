6f62685b0ec9ee19e67fa26566e096ecba9010d2
yxj0312 <yanxiajun@hotmail.com>
2018-12-08 19:36:56 +0100

    Chapter 20 Ep 3 Adding Concerts without Publishing

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,6 @@ Route::group(
         Route::post('/concerts', 'ConcertsController@store');
         Route::get('/concerts/{id}/edit', 'ConcertsController@edit')->name('backstage.concerts.edit');
         Route::patch('/concerts/{id}', 'ConcertsController@update')->name('backstage.concerts.update');
-        Route::post('/published-concerts', 'PublishedConcertsController@store');
+        Route::post('/published-concerts', 'PublishedConcertsController@store')->name('backstage.published-concerts.store');
     }
 );
