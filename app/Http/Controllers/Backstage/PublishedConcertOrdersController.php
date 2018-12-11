bf38365bc1713de60c7cbff027895a83930dc523
yxj0312 <yanxiajun@hotmail.com>
2018-12-11 21:12:50 +0100

    Apply fixes from StyleCI (#47)

--- app/Http/Controllers/Backstage/PublishedConcertOrdersController.php
+++ app/Http/Controllers/Backstage/PublishedConcertOrdersController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers\Backstage;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PublishedConcertOrdersController extends Controller
 {
     public function index($concertId)
     {
         $concert = Auth::user()->concerts()->published()->findOrFail($concertId);
+
         return view('backstage.published-concert-orders.index', [
             'concert' => $concert,
         ]);
