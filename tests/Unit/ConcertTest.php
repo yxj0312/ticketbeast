ec9add1f712bd613759341ce3ccacd271c9e18af
XiajunYan <yanxiajun@hotmail.com>
2018-12-12 17:27:02 +0100

    Chapter 21 Ep 1 Calculating Tickets Sold

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -82,10 +82,20 @@ class ConcertTest extends TestCase
     function tickets_remaing_does_not_include_tickets_associated_with_an_order()
     {
         $concert = factory(Concert::class)->create();
-        $concert->tickets()->saveMany(factory(Ticket::class, 30)->create(['order_id' => 1]));
-        $concert->tickets()->saveMany(factory(Ticket::class, 20)->create(['order_id' => null]));
+        $concert->tickets()->saveMany(factory(Ticket::class, 3)->create(['order_id' => 1]));
+        $concert->tickets()->saveMany(factory(Ticket::class, 2)->create(['order_id' => null]));
 
-        $this->assertEquals(20, $concert->ticketsRemaining());
+        $this->assertEquals(2, $concert->ticketsRemaining());
+    }
+
+    /** @test */
+    function tickets_sold_only_includes_tickets_associated_with_an_order()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->tickets()->saveMany(factory(Ticket::class, 3)->create(['order_id' => 1]));
+        $concert->tickets()->saveMany(factory(Ticket::class, 2)->create(['order_id' => null]));
+
+        $this->assertEquals(3, $concert->ticketsSold());
     }
 
     /** @test */
@@ -152,5 +162,4 @@ class ConcertTest extends TestCase
 
         $this->fail("Reserving tickets succeeded even though the tickets were already reserved.");
     }
-
 }
