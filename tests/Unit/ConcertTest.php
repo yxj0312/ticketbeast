fe42b78f60af1f6ccd2ca786728e5ce7d15f5847
XiajunYan <yanxiajun@hotmail.com>
2018-12-14 17:40:17 +0100

    Some missing changes, maybe I forget to push at home?

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -99,6 +99,25 @@ class ConcertTest extends TestCase
     }
 
     /** @test */
+    function total_tickets_includes_all_tickets()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->tickets()->saveMany(factory(Ticket::class, 3)->create(['order_id' => 1]));
+        $concert->tickets()->saveMany(factory(Ticket::class, 2)->create(['order_id' => null]));
+        $this->assertEquals(5, $concert->totalTickets());
+    }
+    
+    /** @test */
+    function calculating_the_percentage_of_tickets_sold()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->tickets()->saveMany(factory(Ticket::class, 2)->create(['order_id' => 1]));
+        $concert->tickets()->saveMany(factory(Ticket::class, 5)->create(['order_id' => null]));
+        // $this->assertEquals(0.285714286, $concert->percentSoldOut(), '', 0.00001);
+        $this->assertEquals(28.57, $concert->percentSoldOut());
+    }
+
+    /** @test */
     function trying_to_reserve_more_tickets_than_remain_throws_an_exception()
     {
         $concert = ConcertFactory::createPublished(['ticket_quantity' => 10]);
@@ -162,4 +181,6 @@ class ConcertTest extends TestCase
 
         $this->fail("Reserving tickets succeeded even though the tickets were already reserved.");
     }
+
+
 }
