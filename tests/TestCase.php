838a7482d4137ff98f0c66d4faa6f30d65416fa4
yxj0312 <yanxiajun@hotmail.com>
2018-12-16 18:41:17 +0100

    Chapter 22 Ep 2 Asserting Against Sort Order

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 
 abstract class TestCase extends BaseTestCase
@@ -28,6 +29,22 @@ abstract class TestCase extends BaseTestCase
 
         TestResponse::macro('assertViewIs', function ($name) {
             Assert::assertEquals($name, $this->original->name());
+        });
+
+        EloquentCollection::macro('assertContains', function ($value) {
+            Assert::assertTrue($this->contains($value), "Failed asserting that the collection contains the specified value.");
+        });
+
+        EloquentCollection::macro('assertNotContains', function ($value) {
+            Assert::assertFalse($this->contains($value), "Failed asserting that the collection does not contain the specified value.");
+        });
+
+        EloquentCollection::macro('assertEquals', function ($items) {
+            Assert::assertEquals(count($this), count($items));
+            $this->zip($items)->each(function ($pair) {
+                list($a, $b) = $pair;
+                Assert::assertTrue($a->is($b));
+            });
         });
     }
 }
