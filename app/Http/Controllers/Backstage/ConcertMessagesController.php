f2ffd3750009cb90037fd7423a9fdabd5c36272a
yxj0312 <yanxiajun@hotmail.com>
2018-12-19 18:13:49 +0100

    add authorization and validation tests

--- app/Http/Controllers/Backstage/ConcertMessagesController.php
+++ app/Http/Controllers/Backstage/ConcertMessagesController.php
@@ -18,6 +18,11 @@ class ConcertMessagesController extends Controller
     {
         $concert = Auth::user()->concerts()->findOrFail($id);
 
+        $this->validate(request(), [
+            'subject' => ['required'],
+            'message' => ['required'],
+        ]);
+
         $message = $concert->attendeeMessages()->create(request(['subject', 'message']));
 
         return redirect()->route('backstage.concert-messages.new', $concert)
