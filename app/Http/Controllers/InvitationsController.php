9d4925a3ea124387ac7149c12cc40525c7ecbaac
yxj0312 <yanxiajun@hotmail.com>
2019-01-02 22:09:58 +0100

    Apply fixes from StyleCI (#57)

--- app/Http/Controllers/InvitationsController.php
+++ app/Http/Controllers/InvitationsController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Invitation;
-use Illuminate\Http\Request;
 
 class InvitationsController extends Controller
 {
     public function show($code)
     {
         $invitation = Invitation::findByCode($code);
+
         return view('invitations.show', [
             'invitation' => $invitation,
         ]);
