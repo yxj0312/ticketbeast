f24e171b099c7c2181b0a7ba262d1d935a2e23c0
yxj0312 <yanxiajun@hotmail.com>
2019-01-08 21:06:52 +0100

    Apply fixes from StyleCI (#61)

--- app/Mail/InvitationEmail.php
+++ app/Mail/InvitationEmail.php
@@ -5,13 +5,13 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class InvitationEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
     public $invitation;
+
     /**
      * Create a new message instance.
      *
