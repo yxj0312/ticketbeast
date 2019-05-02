4a831b8d527939f2238884a8bccb6cdaaf0dccaf
XiajunYan <yanxiajun@hotmail.com>
2019-05-02 16:43:03 +0200

    Remove unused usage

--- tests/Unit/Mail/InvitationEmailTest.php
+++ tests/Unit/Mail/InvitationEmailTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Unit\Mail;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Invitation;
 use App\Mail\InvitationEmail;
 
